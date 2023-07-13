76e6579cab31ccf0990868ec430febb92dc48bb7
diegosno <dserranoleal@gmail.com>
2023-07-13 16:52:28 +0200

    Add contact template

--- project4/website/views.py
+++ project4/website/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse('<h1>Home</h1>')
+    return render(request, 'website/index.html')
 
 def contact(request):
     return HttpResponse('<h1>Contact</h1>')
