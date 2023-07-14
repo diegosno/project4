f8c68347880a764eda78ac6d7a2be1a9514405da
diegosno <dserranoleal@gmail.com>
2023-07-14 13:57:43 +0200

    Define a New model with fields for title, content, date_created, and author.

--- project4/website/models.py
+++ project4/website/models.py
@@ -3,3 +3,8 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 
 # Create your models here.
+class New(models.Model):
+    title = models.CharField(max_length=40)
+    content = models.TextField()
+    dated_created = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
