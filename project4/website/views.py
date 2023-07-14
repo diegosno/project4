27f49131e3b3d350526088eca12f6217b7599be5
diegosno <dserranoleal@gmail.com>
2023-07-14 17:31:14 +0200

    Register the New model in the Django admin site.

--- project4/website/views.py
+++ project4/website/views.py
@@ -2,21 +2,6 @@ from django.shortcuts import render
 from .models import New
 # Create your views here.
 
-news = [
-    {
-        'title': 'Summer',
-        'author': 'Diego',
-        'content': 'Summer content',
-        'date_created': 'July 12, 2022'
-        }
-    ,
-    {'title': 'Brunch',
-        'author': 'Dany',
-        'content': 'Brunch content',
-        'date_created': 'July 13, 2022'
-        
-    }  
-]
 
 def index(request):
     context = {
