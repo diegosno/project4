2ebf4db949ec1fc4de463add27f911ae40c517b3
diegosno <dserranoleal@gmail.com>
2023-07-15 16:53:04 +0200

    Define a signUp view that renders the signup.html template with the UserCreationForm.

--- project4/users/views.py
+++ project4/users/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 
-
+def signUp(request):
+    form = UserCreationForm()
+    return render(request, 'users/signup.html', {'form': form} )
     
 
 # Create your views here.
