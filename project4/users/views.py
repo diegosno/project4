fca57bc29a1ccd4e25cc51d9cb0aaa125250c9f1
diegosno <dserranoleal@gmail.com>
2023-07-20 14:58:55 +0200

    Validate the form, and display a success message upon successful account creation.

--- project4/users/views.py
+++ project4/users/views.py
@@ -5,6 +5,11 @@ from django.contrib import messages
 def signUp(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Welcome {username}! Your account has been created')
+            return redirect('website-index')
+
     else:
         form = UserCreationForm()
     return render(request, 'users/signup.html', {'form': form} )
