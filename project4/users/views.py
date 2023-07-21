93ac6de162e699bfee76298333a13c70e7278536
diegosno <dserranoleal@gmail.com>
2023-07-21 16:41:55 +0200

    Import the signUpForm from forms.py

--- project4/users/views.py
+++ project4/users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from forms import signUpForm
 
 def signUp(request):
     if request.method == 'POST':
