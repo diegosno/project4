3d7513286c18aa1cf7cfa07ee96fc05ebe907481
diegosno <dserranoleal@gmail.com>
2023-07-24 12:43:58 +0200

    Add 'bio' field as TextField with blank=True and null=True to the Profile model, and define str method to display the Profile as the user's username.

--- project4/users/models.py
+++ project4/users/models.py
@@ -5,7 +5,8 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     picture = models.ImageField(default='default.jpg', upload_to='user_pictures')
-
+    bio = models.TextField(blank=True, null=True)
     
- 
+    def __str__(self):
+        return f'{self.user.username} Profile'
     
