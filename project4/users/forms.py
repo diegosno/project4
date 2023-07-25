b45614994c3a4fe6253a7ad19862636a19c18585
diegosno <dserranoleal@gmail.com>
2023-07-25 12:18:09 +0200

    Import 'Profile' model in the forms module.

--- project4/users/forms.py
+++ project4/users/forms.py
@@ -1,6 +1,7 @@
 from django import forms 
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from. models import Profile 
 
 class signUpForm(UserCreationForm):
     email = forms.EmailField()
@@ -8,3 +9,8 @@ class signUpForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+        
+class updateProfile(forms.ModelForm):
+    email = forms.EmailField()
+
+    
