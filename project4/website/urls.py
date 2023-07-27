7689325ec8a5e61fe2d182640c10bc0446b04e24
diegosno <dserranoleal@gmail.com>
2023-07-27 09:29:48 +0200

    Update URL patterns for views

--- project4/website/urls.py
+++ project4/website/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import NewListView
+from .views import NewListView, NewDetailView
 from . import views 
 
 urlpatterns = [
     path('', NewListView.as_view(), name='website-index'),
+    path('new/<int:pk>/', NewDetailView.as_view(), name='new-detail'),
     path('contact/', views.contact, name='website-contact'),
 ]
