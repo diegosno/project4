545e26df7b0602f1b638154d14e7c16228f5f893
diegosno <dserranoleal@gmail.com>
2023-07-27 17:03:26 +0200

    Add user permission check in NewUpdateView

--- project4/website/urls.py
+++ project4/website/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import NewListView, NewDetailView, NewCreateView
+from .views import NewListView, NewDetailView, NewCreateView, NewUpdateView
 from . import views 
 
 urlpatterns = [
     path('', NewListView.as_view(), name='website-index'),
     path('new/<int:pk>/', NewDetailView.as_view(), name='new-detail'),
+    path('new/<int:pk>/update', NewUpdateView.as_view(), name='new-update'),
     path('new/new/', NewCreateView.as_view(), name='new-create'),
     path('contact/', views.contact, name='website-contact'),
 ]
--- project4/website/views.py
+++ project4/website/views.py
@@ -1,8 +1,9 @@
+from typing import Any, Callable, Optional
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from .models import New
 from django.urls import reverse
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 # Create your views here.
 
 
@@ -28,6 +29,21 @@ class NewCreateView(LoginRequiredMixin, CreateView):
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+    
+    def get_success_url(self):
+        return reverse('website-index')
+    
+class NewUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = New
+    fields = ['title', 'content']
+    
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+    def test_func(self):
+        return self.request.user == self.get_object().author
+
     
     def get_success_url(self):
         return reverse('website-index')
