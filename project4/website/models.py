60ec91d91b61229e1d73cb034f89d51518bd173e
diegosno <dserranoleal@gmail.com>
2023-07-27 16:18:18 +0200

    Add get_absolute_url() method to New model

--- project4/website/models.py
+++ project4/website/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 # Create your models here.
 class New(models.Model):
@@ -11,3 +12,7 @@ class New(models.Model):
     
     def __str__(self):
         return self.title
+    
+    def get_absolute_url(self):
+        return reverse("new-detail", kwargs={"pk": self.pk})
+    
