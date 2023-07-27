16caa99a44c8fc9a92b946aa607df51acb0d7bb5
diegosno <dserranoleal@gmail.com>
2023-07-27 15:16:54 +0200

    Add URL patterns for NewCreateView

--- project4/website/urls.py
+++ project4/website/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import NewListView, NewDetailView
+from .views import NewListView, NewDetailView, NewCreateView
 from . import views 
 
 urlpatterns = [
     path('', NewListView.as_view(), name='website-index'),
     path('new/<int:pk>/', NewDetailView.as_view(), name='new-detail'),
+    path('new/new/', NewCreateView.as_view(), name='new-create'),
     path('contact/', views.contact, name='website-contact'),
 ]
